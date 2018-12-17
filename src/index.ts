91fde3469511c0debfa24c3183752a16ac78981a
Ricky Patel <rickyp@snowcoders.com>
2018-12-16 20:38:52 -0800

    Removed console log statements

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,7 @@ import npmWhich from "npm-which";
 
 export class ExecIfExists {
   public static run(args: string[]) {
-    console.log(`Args: ${args.join("_")}`);
-
     let currentDir = process.cwd();
-    console.log(`currentDir: ${currentDir}`);
 
     let which = npmWhich(currentDir);
     let bin = null;
@@ -15,7 +12,6 @@ export class ExecIfExists {
     } catch (e) {
       return;
     }
-    console.log(`bin: ${bin}`);
 
     execa.sync(bin, args.slice(1), {
       cwd: currentDir
