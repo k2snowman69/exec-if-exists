d0069bd9b755f57c4eeedc8dd99d0091c5652d73
k2snowman69 <rickyp@snowcoders.com>
2021-05-11 01:22:56 -0700

    Improved runtime performance

--- src/programInBin.ts
+++ src/programInBin.ts
@@ -1,25 +1,19 @@
 import fs from "fs";
+import path from "path";
 
 export function programInBin(binDir: string, programName: string) {
   return new Promise<boolean>((resolve, reject) => {
-    fs.readdir(binDir, (err, files) => {
+    const filePath = path.join(binDir, programName);
+    fs.stat(filePath, (err, stats) => {
       if (err != null) {
-        reject(`Error ${err.message ?? err}`);
-        return;
+        return reject(`Error ${err.message ?? err}`);
       }
 
-      if (files == null || files.length === 0) {
-        reject("Error: No node programs found. Did you run npm install?");
-        return;
+      if (stats.isFile()) {
+        return resolve(true);
       }
 
-      if (files.indexOf(programName) !== -1) {
-        resolve(true);
-        return;
-      }
-
-      resolve(false);
-      return;
+      return resolve(false);
     });
   });
 }
