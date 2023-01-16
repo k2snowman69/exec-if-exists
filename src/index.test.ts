66cee3dd4bf0065c16cb26e3389755c9c89b0a2f
k2snowman69 <rickyp@snowcoders.com>
2023-01-15 18:20:40 -0800

    Rewrote internals

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,68 +1,37 @@
-import * as getBinDirectory from "./getBinDirectory";
-import * as programInBin from "./programInBin";
-import * as runNpx from "./runNpx";
-import { execIfExists } from "./index";
-import { mocked } from "ts-jest/utils";
+import { describe, expect, it } from "@jest/globals";
+import { execIfExists } from "./index.js";
 
-jest.mock("./getBinDirectory");
-const mockedGetBinDirectory = mocked(getBinDirectory);
-jest.mock("./programInBin");
-const mockedProgramInBin = mocked(programInBin);
-jest.mock("./runNpx");
-const mockedRunNpx = mocked(runNpx);
-
-beforeEach(() => {
-  mockedGetBinDirectory.getBinDirectory.mockImplementationOnce(() => {
-    return Promise.resolve("node_modules/.bin");
+describe("Error scenarios", () => {
+  it.each<{
+    style: string;
+    value: unknown;
+  }>`
+    style                | value
+    ${"null"}            | ${null}
+    ${"undefined"}       | ${undefined}
+    ${"number"}          | ${1}
+    ${"empty array"}     | ${[]}
+    ${"array of number"} | ${[1]}
+  `("Throws error if no arguments passed - $style", async ({ value }) => {
+    await expect(
+      execIfExists(
+        // @ts-expect-error - Testing invalid inputs, expecting errors here
+        value
+      )
+    ).rejects.toContain("String array");
   });
-  mockedProgramInBin.programInBin.mockImplementationOnce(() => {
-    return Promise.resolve(true);
+});
+
+describe("Success scenarios", () => {
+  it("Returns 0 if program is not found", async () => {
+    await expect(execIfExists(["does-not-exist"])).resolves.toBe(0);
   });
-  mockedRunNpx.runNpx.mockImplementationOnce(() => {
-    return Promise.resolve(0);
+
+  it("Returns 0 on successful execution", async () => {
+    await expect(execIfExists(["npm", "-v"])).resolves.toBe(0);
   });
-});
 
-afterEach(() => {
-  mockedGetBinDirectory.getBinDirectory.mockReset();
-  mockedProgramInBin.programInBin.mockReset();
-  mockedRunNpx.runNpx.mockReset();
-});
-
-it("Does not call mockedGetBinDirectory if arguments are passed", async () => {
-  await execIfExists([]);
-
-  await expect(mockedGetBinDirectory.getBinDirectory).not.toHaveBeenCalled();
-});
-
-it("Returns 0 if getBinDirectory fails", async () => {
-  mockedGetBinDirectory.getBinDirectory
-    .mockReset()
-    .mockImplementationOnce(() => Promise.reject("bleh"));
-  await expect(execIfExists(["sortier"])).resolves.toEqual(0);
-});
-
-it("Returns 0 if programInBin fails", async () => {
-  mockedProgramInBin.programInBin
-    .mockReset()
-    .mockImplementationOnce(() => Promise.reject("bleh"));
-  await expect(execIfExists(["sortier"])).resolves.toEqual(0);
-});
-
-it("Returns 0 if runNpx returns 0 (program succeeded)", async () => {
-  await expect(execIfExists(["sortier"])).resolves.toEqual(0);
-});
-
-it("Returns 0 if programInBin returns false (program not installed scenario)", async () => {
-  mockedProgramInBin.programInBin
-    .mockReset()
-    .mockImplementationOnce(() => Promise.resolve(false));
-  await expect(execIfExists(["sortier"])).resolves.toEqual(0);
-});
-
-it("Returns 50 if runNpx returns 50 (program returned an error code)", async () => {
-  mockedRunNpx.runNpx
-    .mockReset()
-    .mockImplementationOnce(() => Promise.resolve(50));
-  await expect(execIfExists(["sortier"])).resolves.toEqual(50);
+  it("Returns not 0 on non-successful execution", async () => {
+    await expect(execIfExists(["npm", "--flag-does-not-exist"])).resolves.not.toBe(0);
+  });
 });
